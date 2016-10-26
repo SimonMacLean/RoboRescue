decb70d8f7052a282f1e89a7eb89acb87061c33f
SimonMacLean <simon@teammaclean.net>
2016-10-25 20:39:33 -0700

    Added Xamarin files

---
+++ RoboRescue/RoboRescue/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace RoboRescue
+{
+    [Activity(Label = "RoboRescue", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+    public class Person
+    {
+        bool canSwim;
+        int X;
+        int Y;
+    }
+    public class Claw
+    {
+        int X;
+        int Y;
+    }
+}
+
